05ba3b224ae81ab09b498069002934c7f9d026c9
raphael.peim <raphael.peim@etu.imt-lille-douai.fr>
2021-02-02 08:16:25 +0100

    Update web first_app

--- Web/TP/first_app/lib/main.dart
+++ Web/TP/first_app/lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          // child: Text('Hello World'),
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
