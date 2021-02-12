dd596370c62f248f483bace3719e7541780f56eb
raphael.peim <raphael.peim@etu.imt-lille-douai.fr>
2021-02-12 11:23:41 +0100

    Update exercice 4

--- Web/TP/tp2/lib/exercice4.dart
+++ Web/TP/tp2/lib/exercice4.dart
@@ -1,35 +1,60 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 
-class Exercice4 extends StatefulWidget {
-  Exercice4({Key key}) : super(key: key);
+class Tile {
+  Image image;
+  Alignment alignment;
 
-  @override
-  _Exercice4State createState() => _Exercice4State();
+  Tile({this.image, this.alignment});
+
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 0.3,
+            heightFactor: 0.3,
+            child: image,
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _Exercice4State extends State<Exercice4> {
+Tile tile = new Tile(
+    image: Image(image: AssetImage('images/starwars.jpg')),
+    alignment: Alignment(0, 0));
+
+class Exercice4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Exercice 4',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-            appBar: AppBar(
-              leading: BackButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeWidget(),
-                    ),
-                  );
-                },
-              ),
-              title: Text('Exercice 4'),
-            ),
-            body: Center(child: Text('Text'))));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercice 4'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(children: [
+        SizedBox(
+            width: 150.0,
+            height: 150.0,
+            child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: this.createTileWidgetFrom(tile))),
+        Container(
+            height: 200, child: Image(image: AssetImage('images/starwars.jpg')))
+      ])),
+    );
+  }
+
+  Widget createTileWidgetFrom(Tile tile) {
+    return InkWell(
+      child: tile.croppedImageTile(),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
   }
 }
