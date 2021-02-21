fd2445656a2c625c4286fd463c5dd8fd4d50cd28
raphael.peim <raphael.peim@etu.imt-lille-douai.fr>
2021-02-21 15:14:10 +0100

    Edit exercice4.dart

--- Web/TP/tp2/lib/exercice4.dart
+++ Web/TP/tp2/lib/exercice4.dart
@@ -1,27 +1,5 @@
 import 'package:flutter/material.dart';
-
-class Tile {
-  Image image;
-  Alignment alignment;
-
-  Tile({this.image, this.alignment});
-
-  Widget croppedImageTile() {
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: ClipRect(
-        child: Container(
-          child: Align(
-            alignment: this.alignment,
-            widthFactor: 1 / 3,
-            heightFactor: 1 / 3,
-            child: image,
-          ),
-        ),
-      ),
-    );
-  }
-}
+import 'tile.dart';
 
 Tile tile = new Tile(
     image: Image(image: AssetImage('images/starwars.jpg')),
