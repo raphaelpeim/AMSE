b9acc06a1cb8ed7a68d4f709af2a4e0767371c8e
raphael.peim <raphael.peim@etu.imt-lille-douai.fr>
2021-02-22 01:03:17 +0100

    Update exercice3.dart

--- Web/TP/tp2/lib/exercices/exercice3.dart
+++ Web/TP/tp2/lib/exercices/exercice3.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
+import 'exercice1.dart';
+import 'exercice2.dart';
+import 'exercice4.dart';
+import 'exercice5a.dart';
+import 'exercice5b.dart';
+import 'exercice5c.dart';
 
 class Exercice3 extends StatelessWidget {
+  final String _title = 'TP2';
+  final List<String> titles = <String>[
+    'Exercice 1',
+    'Exercice 2',
+    'Exercice 4',
+    'Exercice 5a',
+    'Exercice 5b',
+    'Exercice 5c',
+  ];
+  final List<String> subtitles = <String>[
+    'Show an image',
+    'Transform an image',
+    'Display a tile',
+    'Grid of colored tiles',
+    'Grid based on an image',
+    'Grid with size setable',
+  ];
+  final List<Widget> widgets = <Widget>[
+    Exercice1(),
+    Exercice2(),
+    Exercice4(),
+    Exercice5a(),
+    Exercice5b(),
+    Exercice5c(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Exercice 3'),
+    return MaterialApp(
+        title: _title,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: Center(child: Text('L\'exerice 3 correspond à l\'accueil')));
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('TP2'),
+            ),
+            body: ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: subtitles.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(titles[index]),
+                    subtitle: Text(subtitles[index]),
+                    trailing: Icon(Icons.play_arrow),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => widgets[index],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            )));
   }
 }
--- Web/TP/tp2/lib/main.dart
+++ Web/TP/tp2/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'exercices/exercice3.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomeWidget();
+    return Exercice3();
   }
 }
