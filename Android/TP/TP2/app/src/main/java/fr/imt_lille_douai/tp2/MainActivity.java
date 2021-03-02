6bd15789de11f32a1e4f8c839d4dcd3c3216d3a8
raphael.peim <raphael.peim@etu.imt-lille-douai.fr>
2021-03-02 10:47:27 +0100

    Edit Android TP2

--- Android/TP/TP2/app/src/main/java/fr/imt_lille_douai/tp2/MainActivity.java
+++ Android/TP/TP2/app/src/main/java/fr/imt_lille_douai/tp2/MainActivity.java
@@ -18,16 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView asteroid1 = findViewById(R.id.asteroid1);
-        ImageView asteroid2 = findViewById(R.id.asteroid2);
-        ImageView spaceShip = findViewById(R.id.spaceShip);
-        ImageView button = findViewById(R.id.button);
+        //ImageView asteroid1 = findViewById(R.id.asteroid1);
+        //ImageView asteroid2 = findViewById(R.id.asteroid2);
+        //ImageView spaceShip = findViewById(R.id.spaceShip);
+        //ImageView button = findViewById(R.id.button);
 
-        Path path = new Path();
-        path.arcTo(0f, 0f , 1000f, 1000f, 0f , 359f, true);
-        ObjectAnimator animatorsOfAsteroid = ObjectAnimator.ofFloat(asteroid1, View.X, View.Y, path);
-        animatorsOfAsteroid.setDuration(4000);
-        animatorsOfAsteroid.setRepeatCount(Animation.INFINITE);
-        animatorsOfAsteroid.start ();
+        //Path path = new Path();
+        //path.arcTo(0f, 0f , 1000f, 1000f, 0f , 359f, true);
+        //ObjectAnimator animatorsOfAsteroid = ObjectAnimator.ofFloat(asteroid1, View.X, View.Y, path);
+        //animatorsOfAsteroid.setDuration(4000);
+        //animatorsOfAsteroid.setRepeatCount(Animation.INFINITE);
+        //animatorsOfAsteroid.start();
     }
 }
